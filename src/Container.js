d2461511b47f5f896538a11c3495faa953e8fd99
Shrikrishnaprasad <47346271+Shrikrishnaprasad@users.noreply.github.com>
2021-11-04 15:21:21 +0530

    added Map marker

--- src/Container.js
+++ src/Container.js
@@ -1,6 +1,8 @@
-import { Fragment, useState } from "react";
+import { Fragment, useEffect, useRef, useState } from "react";
 import { useGlobalContext } from "./context";
 import LocationList from "./LocationList";
+import MyMap from "./MyMap";
+import PlacesAutocomplete, { getLatLng } from "react-places-autocomplete";
 
 export default function Container() {
   const {
@@ -9,9 +11,16 @@ export default function Container() {
     coordinates,
     setCoordinates
   } = useGlobalContext();
-  const [lat, setLat] = useState(12.972442);
-  const [lan, setLan] = useState(77.580643);
 
+  const [lat, setLat] = useState(0);
+  const [lan, setLan] = useState(0);
+  // const handleChange = (value) => {
+  //   setLocation(value);
+  // };
+
+  // const handleSelect = (value) => {
+  //   setLocation(value);
+  // };
   const addCoordinates = () => {
     setCoordinates((prev) => {
       if (prev.length === 5) {
@@ -20,10 +29,27 @@ export default function Container() {
       return [...prev, { location, lat, lan }];
     });
     setLocation("");
+    setLat(0);
+    setLan(0);
   };
 
   const showRoute = () => {};
-
+  function getLatLng() {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=5a09bc6cc263cdbbb192656fd8a68edf`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.coord) {
+          setLat(data.coord.lat);
+          setLan(data.coord.lon);
+        }
+      })
+      .catch((err) => console.error(err));
+  }
+  useEffect(() => {
+    getLatLng();
+  }, [location]);
   return (
     <>
       <div className="main-container">
@@ -31,8 +57,48 @@ export default function Container() {
           <div className="main-input main-input-location">
             <div className="input-label">Location Name</div>
             <div className="inputs">
+              {/* <PlacesAutocomplete
+                value={location}
+                onChange={handleChange}
+                onSelect={handleSelect}
+              >
+                {({
+                  getInputProps,
+                  suggestions,
+                  getSuggestionItemProps,
+                  loading
+                }) => (
+                  <div>
+                    <input
+                      className="location-input inputs input"
+                      value={location}
+                      onChange={(e) => setLocation(e.target.value)}
+                      placeholder="Location"
+                      {...getInputProps()}
+                    />
+
+                    <div>
+                      {loading && <div>Loading...</div>}
+                      {suggestions.map((suggestion) => {
+                        const style = suggestion.active
+                          ? { cursor: "pointer" }
+                          : { cursor: "pointer" };
+
+                        return (
+                          <div
+                            {...getSuggestionItemProps(suggestion, { style })}
+                          >
+                            {suggestion.description}
+                          </div>
+                        );
+                      })}
+                    </div>
+                  </div>
+                )}
+              </PlacesAutocomplete>
+            */}
               <input
-                className="location-input inputs"
+                className="location-input inputs input"
                 value={location}
                 onChange={(e) => setLocation(e.target.value)}
                 placeholder="Location"
@@ -42,13 +108,23 @@ export default function Container() {
           <div className="main-input main-input-latlog">
             <div className="input-label">Enter Lattitude</div>
             <div className="inputs">
-              <input className="latlog-input inputs" placeholder="Lat" />
+              <input
+                className="latlog-input inputs"
+                value={lat === 0 ? "" : lat}
+                placeholder="Lat"
+                readOnly
+              />
             </div>
           </div>
           <div className="main-input main-input-latlog">
             <div className="input-label">Enter Longitude</div>
             <div className="inputs">
-              <input className="latlog-input inputs" placeholder="Lon" />
+              <input
+                className="latlog-input inputs"
+                value={lan === 0 ? "" : lan}
+                placeholder="Lon"
+                readOnly
+              />
             </div>
           </div>
           <div className="main-button ">
@@ -85,7 +161,9 @@ export default function Container() {
               )}
             </div>
           </div>
-          <div className="main-map"></div>
+          <div className="main-map">
+            <MyMap />
+          </div>
         </div>
       </div>
     </>
---
+++ src/MyMap.js
@@ -0,0 +1,130 @@
+import React, { Fragment, useEffect, useState } from "react";
+import {
+  GoogleMap,
+  useJsApiLoader,
+  DirectionsService,
+  DirectionsRenderer,
+  Marker
+} from "@react-google-maps/api";
+import { useGlobalContext } from "./context";
+
+const containerStyle = {
+  width: "100%",
+  height: "100%"
+};
+
+const center = {
+  lat: 20.5937,
+  lng: 78.9629
+};
+
+function MyMap() {
+  const { coordinates } = useGlobalContext();
+  const [list, setList] = useState(coordinates);
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: "AIzaSyBvmOwC_T0nWKRTS4migcze7pBFYw4rpRc"
+  });
+
+  const [map, setMap] = React.useState(null);
+
+  // const [res, setRes] = React.useState(null);
+  // const [directions, setDirections] = useState(null);
+
+  const onLoad = React.useCallback(function callback(map) {
+    const bounds = new window.google.maps.LatLngBounds();
+    map.fitBounds(bounds);
+    setMap(map);
+  }, []);
+
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null);
+  }, []);
+
+  // const directionsCallback = React.useCallback((res) => {
+  //   if (res !== null) {
+  //     if (res.status === "OK") {
+  //       setRes(res);
+  //       console.log(res);
+  //     } else {
+  //       console.log("res: ", res);
+  //     }
+  //   }
+  // }, []);
+
+  useEffect(() => {
+    setList(coordinates);
+  }, [coordinates.length]);
+  return isLoaded ? (
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={center}
+      zoom={4}
+      onLoad={onLoad}
+      onUnmount={onUnmount}
+    >
+      {/* Child components, such as markers, info windows, etc. */}
+      <>
+        {coordinates.map((location, index) => (
+          <Fragment key={index}>
+            <Marker position={{ lat: location.lat, lng: location.lan }} />
+          </Fragment>
+        ))}
+
+        {/* <DirectionsService
+          // required
+          options={{
+            // eslint-disable-line react-perf/jsx-no-new-object-as-prop
+            destination: "coimbatore",
+            origin: "chennai",
+            travelMode: "DRIVING"
+          }}
+          // required
+          callback={directionsCallback}
+          // optional
+          onLoad={(directionsService) => {
+            console.log(
+              "DirectionsService onLoad directionsService: ",
+              directionsService
+            );
+          }}
+          // optional
+          onUnmount={(directionsService) => {
+            console.log(
+              "DirectionsService onUnmount directionsService: ",
+              directionsService
+            );
+          }}
+        /> */}
+
+        {/* {res !== null && (
+          <DirectionsRenderer
+            // required
+            options={{
+              // eslint-disable-line react-perf/jsx-no-new-object-as-prop
+              directions: res
+            }}
+            // optional
+            onLoad={(directionsRenderer) => {
+              console.log(
+                "DirectionsRenderer onLoad directionsRenderer: ",
+                directionsRenderer
+              );
+            }}
+            // optional
+            onUnmount={(directionsRenderer) => {
+              console.log(
+                "DirectionsRenderer onUnmount directionsRenderer: ",
+                directionsRenderer
+              );
+            }}
+          />
+        )} */}
+      </>
+    </GoogleMap>
+  ) : (
+    <></>
+  );
+}
+
+export default React.memo(MyMap);
